9c43773151c6046ce3d06c91179b95d9651323e9
jy-hwang <stratojy@gmail.com>
2024-07-12 20:59:53 +0900

    TDD 방식의 REST API 만들기 -  가게상세2
    1. Repository 를 이용한 처리.
    2. 이런 식으로 코드를 작성하는 게 참 낯설다. 이런 방식에 익숙해 질 수 있을까?

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,6 +1,7 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.Restaurant;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,27 +14,18 @@ import java.util.List;
 @RequestMapping("/restaurants")
 public class RestaurantController {
 
+    private RestaurantRepository restaurantRepository = new RestaurantRepository();
 
     @GetMapping
-    public List<Restaurant> list(){
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        Restaurant restaurant = new Restaurant(1004L, "Bob zip","Seoul");
-        restaurants.add(restaurant);
+    public List<Restaurant> list() {
+        List<Restaurant> restaurants = restaurantRepository.findAll();
 
         return restaurants;
     }
 
     @GetMapping("/{id}")
-    public Restaurant detail(@PathVariable("id") Long id){
-
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        restaurants.add(new Restaurant(1004L, "Bob zip","Seoul"));
-        restaurants.add(new Restaurant(2024L, "Cyber Food","Seoul"));
-
-        Restaurant restaurant = restaurants.stream().filter(
-                r-> r.getId().equals(id)).findFirst().orElse(null);
+    public Restaurant detail(@PathVariable("id") Long id) {
+        Restaurant restaurant = restaurantRepository.findById(id);
 
         return restaurant;
     }
