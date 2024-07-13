2451ef86888de9a28b6d959b578b0bd285815f95
jy-hwang <stratojy@gmail.com>
2024-07-13 13:34:52 +0900

    Front-end 간단히 작업
    1. npm init
    2. npm i --save-dev webpack webpack-cli webpack-dev-server
    3. "start": "webpack-dev-server --port 6070 --mode=development" 명령어 추가
    4. fetch를 이용해서 API 요청 후 데이터를 간단히 화면에 보여주는 것까지만 진행하였음.

---
+++ eatgo-web/src/index.js
@@ -0,0 +1,21 @@
+(async () => {
+  const url = "http://localhost:6060/restaurants";
+  const response = await fetch(url);
+  const restaurants = await response.json();
+  const element = document.getElementById("app");
+
+  element.innerHTML = `
+${restaurants
+  .map(
+    (restaurant) =>
+      `
+  <p>
+  ${restaurant.id}
+  ${restaurant.name}
+  ${restaurant.address}
+  </p>
+`
+  )
+  .join("")}
+`;
+})();
