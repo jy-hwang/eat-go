6f3e51167767f0625c647faaba8a63cd3062e8d7
jy-hwang <stratojy@gmail.com>
2024-07-13 19:42:17 +0900

    메뉴관리
    1. 가게의 메뉴를 추가, 수정, 삭제 를 한꺼번에 처리할 수 있게 작성
    2. bulk update
    3. 메뉴등록에 필요한 몇 가지 정보를 json 파일로 정리
    4. JPA를 사용하여 추가 구현하지 않아도 되는 메서드는 주석처리.
    5. postman 으로 가게 등록 -> 조회 -> 메뉴 등록 -> 메뉴 수정 / 삭제 까지 확인완료

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {
     List<MenuItem> findAllByRestaurantId(Long restaurantId);
+
+    //void deleteById(long id); 이게 있으면 오류발생함. 그래서 주석처리함.
 }
