fb46117f49c9246a6756b848baf0bfb8340526b3
jy-hwang <stratojy@gmail.com>
2024-07-14 15:19:38 +0900

    가게 목록 필터링 - 지역 #3
    1. Admin 모듈에서 생성한 지역 관련 파일을 Customer 쪽에 복사.
    2. Customer 는 지역을 직접적으로 등록하지 않으므로, 관련 메서드 및 테스트는 삭제.

---
+++ eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RegionController.java
@@ -0,0 +1,31 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.application.RegionService;
+import kr.co.fastcampus.eatgo.domain.Region;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/regions")
+public class RegionController {
+
+    private final RegionService regionService;
+
+    @Autowired
+    public RegionController(RegionService regionService) {
+        this.regionService = regionService;
+    }
+
+    @GetMapping
+    public List<Region> list() {
+        List<Region> regions = regionService.getRegions();
+
+        return regions;
+    }
+
+}
