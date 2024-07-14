6957efa6a6bc4296097e974454ee59d13f2555ec
jy-hwang <stratojy@gmail.com>
2024-07-14 11:46:43 +0900

    COMMON 프로젝트 정리
    1. gradle 로 전체 프로젝트를 테스트 하기 위해 설정 추가
    2. 메서드 구현이 하나 빠져서 테스트가 실패하고 있었음..

--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/ReviewRepository.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/ReviewRepository.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface ReviewRepository extends CrudRepository<Review, Long> {
 
+    List<Review> findAll();
+
     List<Review> findAllByRestaurantId(long restaurantId);
 
     Review save(Review review);
