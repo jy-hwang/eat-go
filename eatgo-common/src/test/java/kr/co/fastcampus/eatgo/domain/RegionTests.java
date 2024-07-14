5de554d3ddc4ffae7032718a0b6b9a98ffddc044
jy-hwang <stratojy@gmail.com>
2024-07-14 15:14:38 +0900

    가게 목록 필터링 - 지역 #1
    1. 지역 도메인을 공통으로 사용할 수 있도록 common 모듈에 생성.
    2. Repository 와 Test를 간단히 작성해줌.

---
+++ eatgo-common/src/test/java/kr/co/fastcampus/eatgo/domain/RegionTests.java
@@ -0,0 +1,22 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import kr.co.fastcampus.eatgo.util.LoggingTestWatcher;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@ExtendWith(LoggingTestWatcher.class)
+public class RegionTests {
+
+    @Test
+    @DisplayName("")
+    public void creation() {
+        Region region = Region.builder().name("서울").build();
+
+        assertThat(region.getName(), is("서울"));
+    }
+
+}
