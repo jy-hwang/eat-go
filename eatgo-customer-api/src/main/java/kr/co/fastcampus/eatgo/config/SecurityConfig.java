d831c6ef463541d58cba2c31a73d6246aaf2b25e
jy-hwang <stratojy@gmail.com>
2024-07-14 23:30:41 +0900

    시큐리티 초기 설정
    1. gradle에 spring security 를 추가
    2. 아주 기초적인 초기 설정만 해주었음.

---
+++ eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package kr.co.fastcampus.eatgo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/**").permitAll()  // 모든 경로 접근 허용
+                )
+                .formLogin(form -> form.disable())
+                .headers(headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.disable())  // X-Frame-Options 비활성화
+                )
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
