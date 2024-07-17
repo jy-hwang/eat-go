151fe5db7d3919d2fbaf4dc98c5d10f300635cdb
jy-hwang <stratojy@gmail.com>
2024-07-17 23:00:50 +0900

    fix. @Repository 어노테이션 추가
    1. 살펴보니 어노테이션이 없었음.
    2. 기능적으로 문제가 있지는 않지만 좀 더 명시적으로 표현해주는게 좋아보임.

--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/CategoryRepository.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/CategoryRepository.java
@@ -1,9 +1,11 @@
 package kr.co.fastcampus.eatgo.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
     List<Category> findAll();
--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/ReservationRepository.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/ReservationRepository.java
@@ -1,7 +1,9 @@
 package kr.co.fastcampus.eatgo.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
 
     Reservation save(Reservation reservation);
